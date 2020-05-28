fffbc17464e0257d3f88eab0809071ce7ccd26e5
Brennen Tatum <bmar566@gmail.com>
2020-05-28 10:56:32 -0500

    finished part 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building Building1 = new Building("111 south st.")
+            {
+                Stories = 3,
+                Width = 200,
+                Depth = 100,
+
+            };
+            Building1.Construct();
+            Building1.Purchase("John Wark");
+
+            Building1.GetDetails();
+
+        }
+
+    }
+}
